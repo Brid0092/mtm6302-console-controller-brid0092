a0c882e432d57cb9f75799f9890e934cc044b3a6
Alicia Brideau <brid0092@algonquinlive.com>
2022-10-04 20:06:08 -0400

    adds css & js

---
+++ script.js
@@ -0,0 +1,33 @@
+//create a function to chnage the  background color
+//set background
+
+const $body = document.querySelector('body')
+function setBackgroundColor(color) {
+    $body.style.backgroundColor = color
+}
+
+//invoke
+//setBackgroundColor('pink')
+
+//set title
+function setTitle(message){
+    const $h1 = document.querySelector('h1')
+    $h1.textContent = message 
+}
+
+//invoke 
+//setTitle('hello')
+
+//setTitle
+function setDescription(message){
+    const $p = document.querySelector('p')
+    $p.textContent = message
+}
+
+//invoke
+//setDescription 
+
+//setFontColor 
+function setFontColor(color) {
+    $body.style.color = color 
+}
